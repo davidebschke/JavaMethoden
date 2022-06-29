90bf28ec86ee97a8cf05aac30c45440c959e65d5
davidebschke <Davidebschke8@gmail.com>
2022-06-29 12:52:02 +0200

    Java Methoden

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 public class Main
 
 {
+    private static void Testmethode()
+    {
+        int Zahl1 = 2;
+        int Zahl2 = 3;
+
+        System.out.println(Zahl1+Zahl2);
+    }
+
+
+
+
 
     public static void main(String[] args) {
 
         // Methoden
-
+    Testmethode();
     System.out.println("Hallo");
 
 
 
     }
+
+
 }
