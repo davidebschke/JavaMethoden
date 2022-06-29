566f299e11f66e16771d435e613421f1c818ed15
davidebschke <Davidebschke8@gmail.com>
2022-06-29 14:25:37 +0200

    Java Methoden (formatiert)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-public class Main
+public class Main {
 
-{
+    public static int Rechnen(int value) {
 
-    public static int Rechnen(int value)
-    {
-
-        return value*2;
+        return value * 2;
 
     }
-
 
 
 //_____________________________________________//
@@ -16,11 +12,9 @@ public class Main
     public static void main(String[] args) {
 
         // Methoden
-    int result=Rechnen(2);
-    System.out.println("");
-    System.out.println("Das Ergebnis der Methode ist "+ result);
-
-
+        int result = Rechnen(2);
+        System.out.println("");
+        System.out.println("Das Ergebnis der Methode ist " + result);
 
 
     }
