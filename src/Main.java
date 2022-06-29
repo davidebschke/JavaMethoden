43268f2dd7c59b97e618c9484775c7d3f8001797
davidebschke <Davidebschke8@gmail.com>
2022-06-29 15:33:52 +0200

    Java Method
    New:
    
    - Method,variable and class name in english and better names

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
-    public static double Math(double value, double Zahl) {
+    public static double math(double value, double number) {
 
-        return ((value * 2) / 3 * Zahl);
+        return ((value * 2) / 3 * number);
 
     }
 
-    public static String Math2(int Zahlen2, int Zahlen) {
-        return ("Summe:"+(Zahlen2 + Zahlen));
+    public static String math2(int number2, int numbers3) {
+        return ("Summe:" + (number2 + numbers3));
 
     }
 
@@ -17,14 +17,14 @@ public class Main {
     public static void main(String[] args) {
 
         // Method
-        double result = Math(32, 2);
+        double result = math(32, 2);
         System.out.println("Das Ergebnis der Methode ist " + result);
 
 
-        System.out.println(Math2(20,30));
+        System.out.println(math2(20, 30));
 
 
-        if ( result> 100) {
+        if (result > 100) {
 
             System.out.println("Das Ergebnis ist über 100");
         } else {
